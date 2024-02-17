e02d0926b30897b2131a1fd6ad61ab814b52be07
Kiem Bui <kiem.z.bui@hawkeye3d.com>
2024-02-17 10:26:49 -0600

    fix sample data

--- InventoryModels/SampleData.cs
+++ InventoryModels/SampleData.cs
@@ -13,8 +13,8 @@ public static class SampleData
     public static Product[] SampleProducts = new Product[]
     {
         new Product() {Id = 0, Name = "Red Bicycle", Price = 11.44m, Min=1, Max=25},
-        new Product() {Id = 0, Name = "Yellow Bicycle", Price = 11.44m, Min=1, Max=25},
-        new Product() {Id = 0, Name = "Blue Bicycle", Price = 11.44m, Min=1, Max=25}
+        new Product() {Id = 1, Name = "Yellow Bicycle", Price = 11.44m, Min=1, Max=25},
+        new Product() {Id = 2, Name = "Blue Bicycle", Price = 11.44m, Min=1, Max=25}
     };
 
     public static Inventory SampleInventory = new Inventory() { AllParts = new(SampleParts), Products = new(SampleProducts) };
