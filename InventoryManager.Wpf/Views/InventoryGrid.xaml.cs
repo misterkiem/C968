88e7705f2b606f8fdee1414d63ed9cceb16a12c7
Kiem Bui <kiem.z.bui@hawkeye3d.com>
2024-02-17 10:19:25 -0600

    extract datagrid on MainWindowCard to InventoryGrid

---
+++ InventoryManager.Wpf/Views/InventoryGrid.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace InventoryManager.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for InventoryGrid.xaml
+    /// </summary>
+    public partial class InventoryGrid : UserControl
+    {
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(InventoryGrid), new PropertyMetadata(null));
+
+        public string IdHeader
+        {
+            get { return (string)GetValue(IdHeaderProperty); }
+            set { SetValue(IdHeaderProperty, value); }
+        }
+        public static readonly DependencyProperty IdHeaderProperty =
+            DependencyProperty.Register("IdHeader", typeof(string), typeof(InventoryGrid), new PropertyMetadata(null));
+
+        public InventoryGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
