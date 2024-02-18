b5fc788f560594af70b00b997446b71678cac17c
Kiem Bui <kiem.z.bui@hawkeye3d.com>
2024-02-18 03:12:23 -0600

    Add dialogservice

---
+++ InventoryManager.Wpf/Services/DialogService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InventoryManager.Wpf.Services
+{
+    public class DialogService : IDialogService
+    {
+        public bool DisplayConfirmMessage(string message, string title)
+        {
+            var result = MessageBox.Show(message, title, MessageBoxButton.OKCancel);
+            if (result == MessageBoxResult.OK) return true;
+            return false;
+        }
+
+        public void DisplayMessage(string message, string title)
+        {
+            MessageBox.Show(message, title);
+        }
+    }
+}
---
+++ InventoryManager.Wpf/Services/IDialogService.cs
@@ -0,0 +1,8 @@
+﻿namespace InventoryManager.Wpf.Services
+{
+    public interface IDialogService
+    {
+        bool DisplayConfirmMessage(string message, string title);
+        void DisplayMessage(string message, string title);
+    }
+}
